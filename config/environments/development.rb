67cb2f4a0ab2d2606988617637db740c0fdecaee
aditya01933 <adityatiwari01933@gmail.com>
2014-11-14 18:26:29 +0530

    user model added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,6 +34,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  #device user authentication
+  # device user authentication
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
