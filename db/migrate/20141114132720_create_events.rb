c59c72865b69ff82a8bd587a94995969c401d082
aditya01933 <adityatiwari01933@gmail.com>
2014-11-14 19:19:42 +0530

    dashboard initials added

--- db/migrate/20141114132720_create_events.rb
+++ db/migrate/20141114132720_create_events.rb
@@ -1,9 +1,9 @@
 class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
-      t.string, :title
-      t.text, :body
-      t.string, :category
+      t.string :title
+      t.text :body
+      t.string :category
       t.date :eventDate
 
       t.timestamps
